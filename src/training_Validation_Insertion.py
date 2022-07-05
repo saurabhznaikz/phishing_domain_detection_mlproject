31d517858061d76736410b7339a011f113cc07c2
Saurabh Naik <naiksaurabhd@gmail.com>
2022-07-05 20:31:27 +0530

    Added method validateMissingValuesInWholeColumn and DataTranformation

--- src/training_Validation_Insertion.py
+++ src/training_Validation_Insertion.py
@@ -1,5 +1,7 @@
 import os
 from datetime import datetime
+
+from src.DataTransform_Training.DataTransformation import dataTransform
 from src.Training_Raw_data_validation.rawValidation import Raw_Data_validation
 # from DataTypeValidation_Insertion_Training.DataTypeValidation import dBOperation
 # from DataTransform_Training.DataTransformation import dataTransform
@@ -11,7 +13,7 @@ from os import listdir
 class train_validation:
     def __init__(self, path):
         self.raw_data = Raw_Data_validation(path)
-        # self.dataTransform = dataTransform()
+        self.dataTransform = dataTransform()
         # self.dBOperation = dBOperation()
         self.file_object = open("Training_Logs/Training_Main_Log.txt", 'a+')
         self.log_writer = logger.App_Logger()
@@ -21,22 +23,20 @@ class train_validation:
             self.log_writer.log(self.file_object, 'Start of Validation on files!!')
             # extracting values from prediction schema
             column_names, noofcolumns = self.raw_data.valuesFromSchema()
-            # # getting the regex defined to validate filename
-            # regex = self.raw_data.manualRegexCreation()
-            # # validating filename of prediction files
-            # self.raw_data.validationFileNameRaw(regex, LengthOfDateStampInFile, LengthOfTimeStampInFile)
             # validating column length in the file
             self.raw_data.validateColumnLength(noofcolumns)
-            # # validating if any column has all values missing
-            # self.raw_data.validateMissingValuesInWholeColumn()
-            # self.log_writer.log(self.file_object, "Raw Data Validation Complete!!")
-            #
-            # self.log_writer.log(self.file_object, "Starting Data Transforamtion!!")
-            # # replacing blanks in the csv file with "Null" values to insert in table
-            # self.dataTransform.replaceMissingWithNull()
-            #
-            # self.log_writer.log(self.file_object, "DataTransformation Completed!!!")
-            #
+            # validating if any column has all values missing
+            self.raw_data.validateMissingValuesInWholeColumn()
+            self.log_writer.log(self.file_object, "Raw Data Validation Complete!!")
+
+            self.log_writer.log(self.file_object, "Starting Data Transforamtion!!")
+            # below function adds quotes to the '?' values in some columns.
+            self.dataTransform.addQuotesToStringValuesInColumn()
+            # replacing blanks in the csv file with "Null" values to insert in table
+            self.dataTransform.replaceMissingWithNull()
+
+            self.log_writer.log(self.file_object, "DataTransformation Completed!!!")
+
             # self.log_writer.log(self.file_object,
             #                     "Creating Training_Database and tables on the basis of given schema!!!")
             # # create database with given name, if present open the connection! Create table with columns given in schema

035140723edb237735df08f5bfe09525a76efd1a
Saurabh Naik <naiksaurabhd@gmail.com>
2022-08-12 20:53:10 +0530

    stage_02_model_selection_and_tuning added

--- src/utils/common_utils.py
+++ src/utils/common_utils.py
@@ -2,10 +2,17 @@ import os
 import shutil
 from pathlib import Path
 import yaml
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from kneed import KneeLocator
+import numpy as np
 import pickle
+import optuna
+from sklearn import linear_model
+from sklearn import ensemble
+import sklearn.svm
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+import xgboost as xgb
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import accuracy_score,roc_auc_score
 
 def read_params(config_path: str)-> dict:
     """
@@ -84,35 +91,6 @@ def correlation(dataset, threshold):
                 col_corr.add(colname)
     return col_corr
 
-def elbow_plot(data,random_state,init,title,xlabel,ylabel,figure_name,curve,direction):
-    """
-                        Method Name: elbow_plot
-                        Description: This method saves the plot to decide the optimum number of clusters to the file.
-                        Output: A picture saved to the directory
-                        On Failure: Raise Exception
-
-                        Written By: Saurabh Naik
-                        Version: 1.0
-                        Revisions: None
-
-    """
-    wcss=[] # initializing an empty list
-    try:
-        for i in range (1,11):
-            kmeans=KMeans(n_clusters=i,init=init,random_state=random_state) # initializing the KMeans object
-            kmeans.fit(data) # fitting the data to the KMeans Algorithm
-            wcss.append(kmeans.inertia_)
-        plt.plot(range(1,11),wcss) # creating the graph between WCSS and the number of clusters
-        plt.title(title)
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        #plt.show()
-        plt.savefig(figure_name) # saving the elbow plot locally
-        # finding the value of the optimum cluster programmatically
-        kn = KneeLocator(range(1, 11), wcss, curve=curve, direction=direction)
-        return kn.knee
-    except Exception as e:
-        raise e
 
 def save_model(model,filename):
         """
@@ -139,3 +117,97 @@ def save_model(model,filename):
 
         except Exception as e:
             raise e
+
+
+def objective(trial,X_train,y_train):
+    """
+                        Method Name: objective
+                        Description: This method is objective function for optuna.
+                        Output: returns roc_auc score and accuracy score of best model based on training data
+                        On Failure: Raise Exception
+
+                        Written By: Saurabh Naik
+                        Version: 1.0
+                        Revisions: None
+
+    """
+    try:
+        classifier_name = trial.suggest_categorical("classifier", ["LogReg", "RandomForest", "SVC", "NaiveBayes",
+                                                                   "decision-tree", "xgb"])
+
+        # Step 2. Setup values for the hyperparameters:
+        if classifier_name == 'LogReg':
+            C = trial.suggest_uniform('C', 0.01, 10)
+            classifier_obj = linear_model.LogisticRegression(C=C)
+
+        elif classifier_name == 'RandomForest':
+            min_samples_split = trial.suggest_int('min_samples_split', 2, 20)
+            min_samples_leaf = trial.suggest_int('min_samples_leaf', 2, 20)
+            classifier_obj = sklearn.ensemble.RandomForestClassifier(min_samples_split=min_samples_split,
+                                                                     min_samples_leaf=min_samples_leaf)
+
+        elif classifier_name == 'SVC':
+            kernel = trial.suggest_categorical('kernel', ['linear', 'poly', 'rbf', 'sigmoid'])
+            classifier_obj = sklearn.svm.SVC(kernel=kernel)
+
+        elif classifier_name == 'NaiveBayes':
+            var_smoothing = trial.suggest_float("var_smoothing", 1e-4, 0.3, log=True)
+            classifier_obj = sklearn.naive_bayes.GaussianNB(var_smoothing=var_smoothing)
+        elif classifier_name == 'decision-tree':
+            max_depth = trial.suggest_int('max_depth', 5, X_train.shape[1])
+            classifier_obj = sklearn.tree.DecisionTreeClassifier(max_depth=max_depth)
+
+        elif classifier_name == 'xgb':
+            alpha = trial.suggest_float('alpha', 1e-4, 1)
+            subsample = trial.suggest_float('subsample', .1, .5)
+            classifier_obj = xgb.XGBClassifier(alpha=alpha, subsample=subsample)
+
+        #Step 3: Scoring method:
+        accuracy = []
+        roc_auc =[]
+        skf = StratifiedKFold(n_splits=10, random_state=None)
+        skf.get_n_splits(X_train, y_train)
+        # X is the feature set and y is the target
+        for train_index, test_index in skf.split(X_train, y_train):
+            X1_train, X1_test = X_train.iloc[train_index], X_train.iloc[test_index]
+            y1_train, y1_test = y_train.iloc[train_index], y_train.iloc[test_index]
+
+            classifier_obj.fit(X1_train, y1_train)
+            prediction = classifier_obj.predict(X1_test)
+            score = accuracy_score(prediction, y1_test)
+            accuracy.append(score)
+            try:
+                score1 = roc_auc_score(prediction, y1_test)
+                roc_auc.append(score1)
+            except ValueError:
+                pass
+        accuracy=np.array(accuracy).mean()
+        roc_auc = np.array(roc_auc).mean()
+
+        return accuracy, roc_auc
+    except Exception as e:
+        raise e
+
+def find_best_model(X_train,y_train):
+    """
+                        Method Name: find_best_model
+                        Description: This method finds the best model based on accuracy and roc_auc_score.
+                        Output: Return the best model hyperparameters
+                        On Failure: Raise Exception
+
+                        Written By: Saurabh Naik
+                        Version: 1.0
+                        Revisions: None
+
+    """
+    try:
+        sampler = optuna.samplers.NSGAIISampler()
+        func = lambda trial: objective(trial, X_train,y_train)
+        study = optuna.create_study(directions=["maximize", "maximize"], sampler=sampler)
+        study.optimize(func, n_trials=10)
+        trial = study.best_trials
+        param = trial[0].params
+        return param
+
+    except Exception as e:
+        raise e
